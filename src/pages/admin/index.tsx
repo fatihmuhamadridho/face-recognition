939a113a2dc7cb946d2b22dd30bd623f30324e69
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-07-01 04:04:41 +0700

    fix: 🐛 wording dashboard admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,12 +1,15 @@
 import { Default } from '@components/templates/default';
 import { Banner } from '@components/molecules/banner';
+import { useAuthContext } from '@components/atoms';
 
 export default function AdminDashboard() {
+  const { user } = useAuthContext();
+
   return (
     <Default title='Dashboard'>
       <Banner
         subTitle="Nikmati kemudahan layanan kami sesuai kebutuhan Anda, pilih dan masuk ke layanan yang tersedia dibawah ini."
-        title="Selamat Datang, Superadmin!"
+        title={`Selamat Datang, ${user.username}!`}
       />
     </Default>
   );
